0c9bcdefac0ce28844da098fde071121bab37167
Eric Chen <zhenxuanchen2@gmail.com>
2024-10-14 17:34:54 -0400

    navbar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,54 +2,69 @@ import React, { useState } from 'react';
 import { IoMenu, IoClose } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import '../styles/Navbar.css'; // Import the CSS for fade-in effect
 
 function Navbar() {
-  let Links = [
-    {name: "Home", link: "/"},
-    {name: "Meet SAC!", link: "/SAC"},
-    {name: "Clubs", link: "/Clubs"},
-    {name: "Event Calendar", link: "/Event"},
-    {name: "FAQ", link: "/FAQ"},
-  ];
-  let [open, setOpen] = useState(false);
+    let Links = [
+        { name: "Home", link: "/" },
+        { name: "Meet SAC!", link: "/SAC" },
+        { name: "Clubs", link: "/Clubs" },
+        { name: "Event Calendar", link: "/Event" },
+        { name: "FAQ", link: "/FAQ" },
+    ];
 
-  return (
-    <div className='z-50 w-screen max-w-full absolute'>
-      <div className='md:flex items-center justify-between text-indigo-900 bg-transparent py-4 md:px-10 px-7'>
-        <img src={logo} alt="Milliken Logo" className='w-[3rem] font-bold cursor-pointer flex items-center font-[Poppins] text-indigo-900' />
+    let [open, setOpen] = useState(false);
 
-        <ul>
-          {!open && (
-            <li>
-              <button onClick={() => setOpen(true)} className='text-3xl absolute right-6 top-4 cursor-pointer md:hidden'>
-                <IoMenu />
-              </button>
-            </li>
-          )}
-          {open && (
-            <li>
-              <button onClick={() => setOpen(false)} className='text-3xl absolute right-6 top-4 cursor-pointer md:hidden'>
-                <IoClose />
-              </button>
-            </li>
-          )}
-        </ul>
+    const handleLinkClick = () => {
+        if (window.innerWidth < 768) { 
+            setOpen(false); 
+        }
+    };
 
-        <ul className={`md:flex md:items-center md:pb-0 absolute md:static backdrop-blur md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in-out ${open ? 'top-14 ' : 'top-[-490px]'}`}>
-          {
-            Links.map((link) => (
-              <li key={link.name} className='md:ml-8 text-2xl md:my-0 my-7'>
-                <Link to={link.link} className='text-indigo-900 p-2 rounded-lg hover:text-blue-100 hover:bg-indigo-900 duration-200'>
-                  {link.name}
+    return (
+        <div className='z-50 w-screen max-w-full absolute'>
+            <div className='md:flex items-center justify-between text-indigo-900 bg-transparent py-4 md:px-10 px-7'>
+                {/* Wrap logo with Link to navigate to home page */}
+                <Link to="/" className='flex items-center' onClick={handleLinkClick}>
+                    <img src={logo} alt="Milliken Logo" className='w-[3rem] font-bold cursor-pointer' />
                 </Link>
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-      <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
-    </div>
-  )
+
+                <ul>
+                    {!open && (
+                        <li>
+                            <button onClick={() => setOpen(true)} className='text-3xl absolute right-6 top-4 cursor-pointer md:hidden'>
+                                <IoMenu />
+                            </button>
+                        </li>
+                    )}
+                    {open && (
+                        <li>
+                            <button onClick={() => setOpen(false)} className='text-3xl absolute right-6 top-4 cursor-pointer md:hidden'>
+                                <IoClose />
+                            </button>
+                        </li>
+                    )}
+                </ul>
+
+                <ul className={`md:flex md:items-center md:pb-0 absolute md:static backdrop-blur md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in-out ${open ? 'top-14 fade-in' : 'top-[-490px]'}`}>
+                    {
+                        Links.map((link) => (
+                            <li key={link.name} className='md:ml-8 text-2xl md:my-0 my-7'>
+                                <Link 
+                                    to={link.link} 
+                                    className='text-indigo-900 p-2 rounded-lg hover:text-blue-100 hover:bg-indigo-900 duration-200'
+                                    onClick={handleLinkClick} // Close on link click
+                                >
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
+        </div>
+    );
 }
 
 export default Navbar;
