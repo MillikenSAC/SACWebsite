99ae34d723392dcc7642a091c50a550a00fec95e
Eric Chen <zhenxuanchen2@gmail.com>
2024-10-13 22:12:47 -0400

    Alphabetical sorting of clubs

--- src/components/ClubCard.js
+++ src/components/ClubCard.js
@@ -1,19 +1,23 @@
+import React from 'react';
+
 export default function ClubCard({ name, desc, imgSrc, link }) {
   return (
       <div
-          onClick={() => window.open(link, '_blank')}
-          className="relative cursor-pointer overflow-hidden rounded-3xl shadow-xl border-indigo-900 border transition-transform hover:scale-105 hover:shadow-2xl duration-200 ease-in-out bg-white p-6 h-80 w-96" // Increased height to h-80 and width to w-96
+          className="relative overflow-hidden rounded-3xl shadow-xl border-indigo-900 border transition-transform hover:scale-105 hover:shadow-2xl duration-200 ease-in-out bg-white p-6 h-80 w-96" // Removed cursor-pointer
       >
           <div className="flex items-start h-full">
-              <img
-                  src={imgSrc}
-                  alt={name}
-                  className="w-20 h-20 rounded-full border-2 border-indigo-900 mr-4" // Increased size of the circular image
-              />
+              <div className="relative">
+                  <img
+                      src={imgSrc}
+                      alt={name}
+                      className="w-20 h-20 rounded-full border-2 border-indigo-900 mr-4 transition-transform duration-200 ease-in-out hover:scale-110 cursor-pointer" // Only logo has cursor-pointer
+                      onClick={() => window.open(link, '_blank')} // Only clicking the logo works
+                  />
+              </div>
               <div className="flex-1">
                   <div className="text-xl font-bold text-sky-950">{name}</div>
                   <div className="h-1 bg-indigo-900 my-2"></div>
-                  <div className="text-gray-700 h-40 overflow-hidden overflow-ellipsis"> {/* Increased height for description */}
+                  <div className="text-gray-700 h-40 overflow-hidden overflow-ellipsis">
                       {desc}
                   </div>
               </div>
--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -4,26 +4,33 @@ import ClubCard from '../components/ClubCard.js';
 import logo from '../assets/logo.png';
 import robotics from '../assets/robotics.png';
 
+const clubData = [
+  { name: "Milliken Robotics", desc: "The best club at Milliken", imgSrc: robotics, link: "https://www.instagram.com/mmhs.robotics/" },
+  { name: "Milliken SAC", desc: "The Milliken Mills High School Student Activity Council", imgSrc: logo, link: "https://www.instagram.com/milliken_sac/" },
+  { name: "Club 3", desc: "Description for Club 3", imgSrc: logo, link: "https://www.instagram.com/milliken_sac/" },
+  { name: "Club 4", desc: "Description for Club 4", imgSrc: logo, link: "https://www.instagram.com/milliken_sac/" },
+  { name: "Club 5", desc: "Description for Club 5", imgSrc: logo, link: "https://www.instagram.com/milliken_sac/" },
+  { name: "Club 6", desc: "Description for Club 6", imgSrc: logo, link: "https://www.instagram.com/milliken_sac/" }
+];
 
 function Clubs() {
+  // Sort clubData alphabetically by club name
+  const sortedClubs = clubData.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
-      <div className="flex w-screen h-fit min-h-screen flex-col max-w-full overflow-x-hidden relative bg-blue-100">
-          <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet"></link>
-          <div className="flex w-fit h-fit place-self-center mt-20 text-center text-sky-950 text-[10rem] font-thin font-['Prata']">
-              clubs
-          </div>
-
-          <div className="grid w-5/6 max-w-full grid-cols-1 clubCardSmall clubCardLarge gap-y-6 place-items-center place-self-center mb-10 mt-5"> {/* Adjusted gap to 6 for smaller spacing */}
-              <ClubCard name="Milliken Robotics" desc="The best club at Milliken" imgSrc={robotics} link="https://www.instagram.com/mmhs.robotics/" />
-              <ClubCard name="Milliken SAC" desc="The Milliken Mills High School Student Activity Council" imgSrc={logo} link="https://www.instagram.com/milliken_sac/" />
-              <ClubCard name="Club 3" desc="Description for Club 3" imgSrc={logo} link="https://www.instagram.com/milliken_sac/" />
-              <ClubCard name="Club 4" desc="Description for Club 4" imgSrc={logo} link="https://www.instagram.com/milliken_sac/" />
-              <ClubCard name="Club 5" desc="Description for Club 5" imgSrc={logo} link="https://www.instagram.com/milliken_sac/" />
-              <ClubCard name="Club 6" desc="Description for Club 6" imgSrc={logo} link="https://www.instagram.com/milliken_sac/" />
-          </div>
+    <div className="flex w-screen h-fit min-h-screen flex-col max-w-full overflow-x-hidden relative bg-blue-100">
+      <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet"></link>
+      <div className="flex w-fit h-fit place-self-center mt-20 text-center text-sky-950 text-[10rem] font-thin font-['Prata']">
+        clubs
       </div>
-  )
-}
 
+      <div className="grid w-5/6 max-w-full grid-cols-1 clubCardSmall clubCardLarge gap-y-6 place-items-center place-self-center mb-10 mt-5">
+        {sortedClubs.map((club, index) => (
+          <ClubCard key={index} name={club.name} desc={club.desc} imgSrc={club.imgSrc} link={club.link} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Clubs;
