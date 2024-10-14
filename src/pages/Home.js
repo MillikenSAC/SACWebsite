06a039a2b6d1393cda88b697d8cd53d3a30afc0c
Eric Chen <zhenxuanchen2@gmail.com>
2024-10-14 13:30:58 -0400

    revised home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import logo from '../assets/robotics.png';
 function Home() {
   return (
     <div class="flex w-screen flex-col max-w-full overflow-x-hidden">
-      <img class="bg-gray-50 w-full h-screen max-w-full z-[-1] absolute" src={sacbg} alt="Background"/>
+      <img class="bg-gray-50 w-screen h-screen max-w-full z-[-1] absolute" src={sacbg} alt="Background"/>
 
     {/* top slide of home page  */}
         <div class="w-screen h-screen overflow-hidden relative flex flex-col place-items-center place-content-center">
@@ -28,15 +28,16 @@ function Home() {
             <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
               <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">
-                  Text here
-                </p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                </p>
+              <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center text-center">
+                UPCOMING EVENTS
+              </p>
+              <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center text-center">
+                there will a list of upcoming big school events and stuff 
+              </p>
+
               </div>
               <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
-              <img src={logo} alt="Robotics Logo" />
+              <img src="" alt="Robotics Logo" />
 
               </div>
             </div>
@@ -45,16 +46,16 @@ function Home() {
           <div className="relative max-lg:row-start-1">
             <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10 text-center">
                 <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">
-                  Text here
+                  MEET SAC
                 </p>
                 <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
+                  maybe some cool graphic of sac 
                 </p>
               </div>
               <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
-              <img src={logo} alt="Robotics Logo" />
+              <img src="" alt="Robotics Logo" />
               </div>
             </div>
             <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
@@ -62,14 +63,14 @@ function Home() {
           <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
             <div className="absolute inset-px rounded-lg bg-white"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
-              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">Text here</p>
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10 text-center">
+                <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">Your Questions Answered</p>
                 <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                   Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
                 </p>
               </div>
               <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
-              <img src={logo} alt="Robotics Logo" />
+              <img src="" alt="Robotics Logo" />
 
               </div>
             </div>
@@ -79,14 +80,15 @@ function Home() {
             <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
               <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center">
-                  Text here
-                </p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
-                </p>
+              <p className="mt-2 text-lg/7 font-medium tracking-tight text-gray-950 max-lg:text-center text-center">
+                CLUBS
+              </p>
+              <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center text-center">
+                there will be stuff inside here 
+              </p>
+
               </div>
-              <img src={logo} alt="Robotics Logo" />
+              <img src="" alt="Robotics Logo" />
             </div>
             <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
           </div>
