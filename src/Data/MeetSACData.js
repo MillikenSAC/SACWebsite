02d8f9d84a08406297ee85a8a2a358740f2cf1a6
Airplane356 <167908754+Airplane356@users.noreply.github.com>
2024-11-16 13:05:45 -0500

    captions

--- src/Data/MeetSACData.js
+++ src/Data/MeetSACData.js
@@ -3,32 +3,36 @@ export const Executives = [
       id: 1,
       img: 'https://images.unsplash.com/photo-1547005327-ef75a6961556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8b2NlYW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Emily Dong',
-      title: 'President'
+      title: 'President',
+      caption: "hii milliken!! my name is emily, your 24-25 sac prez!! this is my third (and final) year on sac 😢 but i truly believe that it is going to be the best one yet 💯.  i like buying overpriced matcha lattes 🍵, my overpriced cat miso 🐈‍⬛, and collecting overpriced jellycats 🧸. we have such an awesome + hard working team this year and i can't wait to begin this new era of student council!! as always, my dms are open for any questions  (@emily.dddd) see u around knights 😛"
     },
     {
       id: 2,
       img: 'https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Steven Tang',
       title: 'Vice-President',
-      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus vel purus sed malesuada. Cras eu erat purus. Duis rhoncus euismod odio.'
+      caption: "Heyyyy! This is Steven Tang your VP! 🎉It's my 3rd year on SAC (feeling like an unc now 😎) but I'm here to make sure this year is 🔥. Got questions or ideas? Slide into my DMs @steventang26. See you guys around and let's make Milliken great again!"
     },
     {
       id: 3,
       img: 'https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Melanie Shen',
-      title: 'Student Advisor'
+      title: 'Student Advisor',
+      caption: "HI MILLIKEN I'm Melanie and I'm thrilled to be on sac again as a student advisor this year😝 if im not panicking about school or grinding semi decor again i love yapping,; brain rot, doing nail art and scrolling on my phone😔If you have ANY questions and suggestions about SAC or just want to yap about life, feel free to dm @_.mels.anie._ 😋"
     },
     {
       id: 4,
       img: 'https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Madelyn Young',
-      title: 'Student Advisor'
+      title: 'Student Advisor',
+      caption: "heyyy team i'm madelyn and i'm one of the three awesome student advisors on sac! when i'm not planning awesome sac activities, i enjoy cooking and doomscrolling 🥰  reach me @madelynyoungg  😚😚"
     },
     {
       id: 5,
       img: 'https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Bhadra Thumpayil',
-      title: 'Student Advisor'
+      title: 'Student Advisor',
+      caption: "hi knights my name is bhadra and i'm part of your amazing student advisor team this year! outside of school, i unfortunately don't do much, but i enjoy reading, finding stickers for my laptop or trying new sidequests. if you have any questions or ideas for sac (or model un) my insta is @bhadra.th. see you around!"
     },
   ];
 
@@ -37,7 +41,8 @@ export const Secretary = [
       id: 6,
       img: 'https://images.unsplash.com/photo-1566024287286-457247b70310?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       name: 'Alan Lin',
-      title: 'Secretary'
+      title: 'Secretary',
+      caption: "What's up Knights 🔥, this is Alan, your one and only SAC secretary this year.  This is my first year on the council, and it has already taught me how to type 300WPM per minute 😵‍💫. Anyways,  I love to talk and network, so feel free to wave me in the halls or dm me at @alanctic. Let's make this year a total blast 😉"
     },
 ];
 
@@ -46,13 +51,15 @@ export const SocialAffairs = [
     id: 7,
     img: 'https://images.unsplash.com/photo-1494791368093-85217fbbf8de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8b2NlYW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Madison C. Mitran',
-    title: 'Internal Affairs'
+    title: 'Internal Affairs',
+    caption: "Hi all! I'm Madison - the internal social affairs coordinator for 2024-25. Questions about clubs or events? Make sure you reach out in school, or send me an email (348601295@gapps.yrdsb.ca) and I'll get back to you ASAP - that is, when I'm not in the pool or reading feminist literature... 💜"
 },
 {
     id: 8,
     img: 'https://images.unsplash.com/photo-1494791368093-85217fbbf8de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8b2NlYW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Mahisa Thayananthan',
-    title: 'External Affairs'
+    title: 'External Affairs',
+    caption: "Hey KNIGHTS!! 📢💥😜 My name is Mahisa and I am one of your social affairs on sac! This is my first year on SAC and I hope to make it special just for everyone else! 🫶✨ If you ever want to just chat or have any questions feel free to dm me @mahisathaya. Some of my favourite things to do is yapping, music, and reading! "
 },
 ]
 
@@ -61,13 +68,15 @@ export const Treasurers = [
     id: 9,
     img: 'https://images.unsplash.com/photo-1494791368093-85217fbbf8de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8b2NlYW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Sam Wang',
-    title: 'Treasurer'
+    title: 'Treasurer',
+    caption: "Wasssup Milliken! I'm Sam, one of your treasurers for this year! 💰💰 It's my first year on SAC and I’m here to make sure we keep that 🍞 up for all the fun stuff this year! Feel free to message me anytime @sam.wwang! Let's get the funds flowing, and good times rolling  💸😎 "
   },
   {
     id: 10,
     img:'https://images.unsplash.com/photo-1494791368093-85217fbbf8de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8b2NlYW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Jenny Cai',
-    title: 'Treasurer'
+    title: 'Treasurer',
+    caption: "Hey Knights! I'm Jenny, your other treasurer for this year! This is also my first year on SAC and I hope to make funds more accessible for student events and planning!"
   },
   ];
 
@@ -76,31 +85,36 @@ export const Publicity = [
         id: 11,
         img: 'https://images.unsplash.com/photo-1551405780-03882d5a2ba7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Anita Tran',
-        title: 'Graphic Publicity'
+        title: 'Graphic Publicity',
+        caption: "Hi guys I really want a dog @anutlord @anitatr4n"
       },
       {
         id: 12,
         img: 'https://images.unsplash.com/photo-1562059392-096320bccc7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Tristin Tran',
-        title: 'Graphic Publicity'
+        title: 'Graphic Publicity',
+        caption: "What is UP my mighty knightys 🔥 Im tristin and I'll be one of your graphic pubs along with the GOAT anita tran #tranandtran4L 🙏 I am a first year saccie (along with my dayone's menglu and mahisa) and am so excited to work with this amazing team to make this year the BEST school year EVER 😳 Feel free to say hi if you see me in the hall, have a chat @s3leryy_ @trisownies or look at my PERFECT letterboxd @ rambeau 👍 "
       },
       {
         id: 13,
         img: 'https://images.unsplash.com/photo-1551405780-03882d5a2ba7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Andrew Li',
-        title: 'Visual Publicity'
+        title: 'Visual Publicity',
+        caption: "Hey knights! I'm andrew and i'll be one of your visual pubs this year. When i'm not staying long hours after school to meet banner deadlines, i love collecting smiskis 🎎 and rock climbing 🧗. say hi @just.andrew.li"
       },
       {
         id: 14,
         img: 'https://images.unsplash.com/photo-1562059392-096320bccc7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Isabelle Zhao',
-        title: 'Visual Publicity'
+        title: 'Visual Publicity',
+        caption: "hey knights I'm Isabelle and i am one of ur visual pubs this year‼️. This is my third year on sac. im really looking forward to graduation 😍😍😻😻❤️‍🔥"
       },
       {
         id: 15,
         img: 'https://images.unsplash.com/photo-1562059392-096320bccc7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Menglu Zhu',
-        title: 'Visual Publicity'
+        title: 'Visual Publicity',
+        caption: "Hey Milliken, I’m Menglu and I’m one of your visual pubs this year. This is my first year on SAC (stoked to be a saccie as Tristin would say). If you ever see me in the halls say hi or dm me @mengluzhuu :D"
       },
   ];
 
@@ -109,31 +123,36 @@ export const Reps = [
         id: 16,
         img: 'https://images.unsplash.com/photo-1478359844494-1092259d93e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Shy Safi',
-        title: 'Grade 12 Rep'
+        title: 'Grade 12 Rep',
+        caption: "heyyy milliken! my name is shy, this years grade 12 (unc) rep. as i approach my last year at milliken, i'll make sure to make it the most memorable yet! feel free to message me @shyysafi 😊"
       },
       {
         id: 17,
         img: 'https://images.unsplash.com/photo-1514999037859-b486988734f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Seyon Satheesh',
-        title: 'Grade 11 Rep'
+        title: 'Grade 11 Rep',
+        caption: "Hey Milliken — My name is Seyon Satheesh and I'm your Grade 11 Rep for this year. Feel free to contact me if you have any questions about SAC or our events."
       },
       {
         id: 18,
         img: 'https://images.unsplash.com/photo-1478359844494-1092259d93e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Noel Ho',
-        title: 'Grade 10 Rep'
+        title: 'Grade 10 Rep',
+        caption: "Hey Knights‼️I'm Noel, this years grade 10 rep🤓 Though it's my first year on SAC, I'm sure we'll make it the BEST year ever. I like singing, drawing, and rotten mango podcasts. If you have any questions or need a yap buddy, hit me up on @noelhohoho on insta🥳🥳"
       },
       {
         id: 19,
         img: 'https://images.unsplash.com/photo-1514999037859-b486988734f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Emma Xu',
-        title: 'Grade 9 Rep'
+        title: 'Grade 9 Rep',
+        caption: "Heyy milliken, I’m Emma! One of the grade 9 reps along with Farhaan! This year is my first of (hopefully) many years on sac, and I'm super excited to make your time at school a blast😋!! Fun facts about me: I swim and run, but I can’t play any other sports😒, I loveee yapping to everyone about anything😁, and my favourite food is strawberries 😍! If you see me in the halls, come and say hi or dm me @emma.10.xu!"
       },
       {
         id: 20,
         img: 'https://images.unsplash.com/photo-1478359844494-1092259d93e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Farhaan Ferdous',
-        title: 'Grade 9 Rep'
+        title: 'Grade 9 Rep',
+        caption: "wassupp knights 🔥🔥🔥, im farhaan, one of the grade 9 reps for this year alongside emma!! this is my first year on sac and i’m here to make it as memorable as possible 😝! some info abt me: i love playing sports (esp badminton 🏸). i also LOVE yapping, so if u see me around, come dab me up, im sure ill be down to chat 🗣. im rlly excited to see how this year unfolds ‼️‼️ feel free to send me a dm anytime on insta (@fxrhaanfxrdous)"
       },
       {
         id: 21,
@@ -148,24 +167,28 @@ export const Tech = [
         id: 22,
         img: 'https://images.unsplash.com/photo-1509477887414-681937645173?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Eric Chen',
-        title: 'Tech Chair'
+        title: 'Tech Chair',
+        caption: "hey milliken! im eric c, your tech chair for this year, and i heavily dislike replit🔥@zxc.eric_ "
       },
       {
         id: 23,
         img: 'https://images.unsplash.com/photo-1454783904586-9fa42a1e8442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Eleanor Liu',
-        title: 'Photo/Videographer'
+        title: 'Photo/Videographer',
+        caption: "hi milliken! im eleanor, the sac photographer/videographer this year and i now have a profound hatred for ios cameras "
       },
       {
         id: 24,
         img: 'https://images.unsplash.com/photo-1530539595977-0aa9890547c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Sophie Ding',
-        title: 'A.V. Rep'
+        title: 'A.V. Rep',
+        caption: "Hey Knights, I'm Sophie, and I'm in charge of audio/visual tech along with Eric S. That means you should COME SEE OUR SHOWS!!! 🔥🔥🔥 I'm always up for a good chat, so you can find me at @daweirddragon or sometimes in the music hallway. See you around!"
       },
       {
         id: 25,
         img: 'https://images.unsplash.com/photo-1542262868-cec49cce6571?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
         name: 'Eric Siu',
-        title: 'A.V. Rep'
+        title: 'A.V. Rep',
+        caption: "Hi milliken, I'm Eric.s, and I'm in charge of audio/visual tech along with Sophie. This is my last year on SAC. I love boba (the store at dension remembers my order) and studying advance functions yippie!! You can find me at @yentah__ or in the drama room if im on my spare."
       },
 ];
